d8d38cf080d1fe4ea6035dcfbd5de16e0177c5d5
Mike Laursen <35746661+mvlaursen@users.noreply.github.com>
2020-02-18 16:21:38 -0700

    Update reasonable.py
    Rough HTML generation

--- reasonable.py
+++ reasonable.py
@@ -6,7 +6,18 @@ def main():
     if len(sys.argv) != 2:
         print("usage: " + os.path.basename(sys.argv[0]) + " <.csv file>")
     else:
-        print("Gonna go to town on: " + sys.argv[1])
+        filename = sys.argv[1]
+        print("Gonna go to town on: " + filename)
+
+        with open(filename, 'r') as csvfile:
+            rows = []
+
+            csvreader = csv.DictReader(csvfile)
+            print ("<html><body><ul>")
+            for row in csvreader:
+                print("<li><a href='" + row['article-link-href'] + "'>" + row['article-title'] + "</a> -- " + row['article-author'] + ": <i>" + row['article-subtitle'] + "</i></li>")
+            print("</ul>")
+            print("</body></html>")
 
 if __name__ == "__main__":
     main()
