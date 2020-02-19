e5ff7c825e8d2e1c791f9ff59b08d1b80db79bb0
Mike Laursen <35746661+mvlaursen@users.noreply.github.com>
2020-02-19 14:09:17 -0700

    Update reasonable.py
    Use correct character sets for i/o.

--- reasonable.py
+++ reasonable.py
@@ -3,28 +3,34 @@ import os
 import sys
 
 def write_li(article, output_file):
-    output_file.write("            <li><a href='" + article['href'] + "'>" + article['title'] + "</a> -- " + article['author'] + ": <i>" + article['subtitle'] + "</i></li>")
+    output_file.write("        <li><a href='" + article['href'] + "'>" + article['title'] + "</a> -- " + article['author'] + ": <i>" + article['subtitle'] + "</i></li>\n")
 
 def write_html(articles, output_file):
-    output_file.write("<html>")
-    output_file.write("    <body>")
-    output_file.write("        <ul>")
+    output_file.write("<!doctype html>\n")
+    output_file.write("<html lang='en'>\n")
+    output_file.write("<head>\n")
+    output_file.write("    <meta charset='utf-8'>\n")
+    output_file.write("</head>\n")
+    output_file.write("<body>\n")
+    output_file.write("    <ul>\n")
 
     for article in articles:
         write_li(article, output_file)
 
-    output_file.write("        </ul>")
-    output_file.write("    </body>")
-    output_file.write("/<html>")
+    output_file.write("    </ul>\n")
+    output_file.write("</body>\n")
+    output_file.write("/<html>\n")
     
 def main():
     if len(sys.argv) != 3:
         print("usage: " + os.path.basename(sys.argv[0]) + " input-filepath.csv output-filepath.html")
+        print("    input-file.csv: Path to an input file created by the Firefox Web Scraper extension.")
+        print("    output-filepath.csv: An HTML file listing the articles in an unordered, bulleted list.")
     else:
         input_filepath = sys.argv[1]
         output_filepath = sys.argv[2]
 
-        with open(input_filepath, 'r') as input_file:
+        with open(input_filepath, 'r', encoding='utf-8') as input_file:
             articles = []
             csv_reader = csv.DictReader(input_file)
             for row in csv_reader:
@@ -35,7 +41,7 @@ def main():
                 article['subtitle'] = row['article-subtitle']
                 articles.append(article)
 
-            with open(output_filepath, 'w') as output_file:
+            with open(output_filepath, 'w', encoding='utf-8') as output_file:
                 write_html(articles, output_file)
 
 if __name__ == "__main__":
