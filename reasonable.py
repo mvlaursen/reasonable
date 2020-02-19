2b67f5f77046ad231618c1325cd8ed342c95102d
Mike Laursen <35746661+mvlaursen@users.noreply.github.com>
2020-02-19 16:31:38 -0700

    Update reasonable.py
    Finished?

--- reasonable.py
+++ reasonable.py
@@ -2,19 +2,14 @@ import csv
 import os
 import sys
 
-SCRAPER_AUTHOR_KEY = 'article-author'
-SCRAPER_HREF_KEY = 'article-link-href'
-SCRAPER_ORDER_KEY = '\ufeffweb-scraper-order'
-SCRAPER_SUBTITLE_KEY = 'article-subtitle'
-SCRAPER_TIME_KEY = 'article-datetime'
-SCRAPER_TITLE_KEY = 'article-title'
-
-AUTHOR_KEY = 'author'
-HREF_KEY = 'href'
-ORDER_KEY = 'order'
-SUBTITLE_KEY = 'subtitle'
-TIME_KEY = 'time'
-TITLE_KEY = 'title'
+# These are the keys that the Firefox Web Scraper extension uses.
+# Some of them may come from Reason's HTML.
+AUTHOR_KEY = 'article-author'
+HREF_KEY = 'article-link-href'
+ORDER_KEY = '\ufeffweb-scraper-order'
+SUBTITLE_KEY = 'article-subtitle'
+TIME_KEY = 'article-datetime'
+TITLE_KEY = 'article-title'
 
 def write_li(article, output_file):
     output_file.write("        <li><a href='" + article[HREF_KEY] + "'>"
@@ -52,15 +47,16 @@ def main():
             csv_reader = csv.DictReader(input_file)
             for row in csv_reader:
                 article = {}
-                article[AUTHOR_KEY] = row[SCRAPER_AUTHOR_KEY]
-                article[HREF_KEY] = row[SCRAPER_HREF_KEY]
-                article[ORDER_KEY] = row[SCRAPER_ORDER_KEY]
-                article[SUBTITLE_KEY] = row[SCRAPER_SUBTITLE_KEY]
-                article[TIME_KEY] = row[SCRAPER_TIME_KEY]
-                article[TITLE_KEY] = row[SCRAPER_TITLE_KEY]
+                article[AUTHOR_KEY] = row[AUTHOR_KEY]
+                article[HREF_KEY] = row[HREF_KEY]
+                article[ORDER_KEY] = row[ORDER_KEY]
+                article[SUBTITLE_KEY] = row[SUBTITLE_KEY]
+                article[TIME_KEY] = row[TIME_KEY]
+                article[TITLE_KEY] = row[TITLE_KEY]
                 articles.append(article)
 
             articles.sort(key = lambda i: (i[ORDER_KEY]), reverse=True)
+            articles = filter(lambda i: (not i[TITLE_KEY].startswith(("Brickbat", "Review"))), articles)
 
             with open(output_filepath, 'w', encoding='utf-8') as output_file:
                 write_html(articles, output_file)
