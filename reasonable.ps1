1792a125df3d7a650a56a0a8ca9f6060a52e755c
Mike Laursen <35746661+mvlaursen@users.noreply.github.com>
2020-02-19 17:15:32 -0700

    Update reasonable.ps1
    Do even more with PowerShell script.

--- reasonable.ps1
+++ reasonable.ps1
@@ -1,1 +1,3 @@
+Start-Process -FilePath "C:\Program Files\Mozilla Firefox\firefox.exe" -ArgumentList "-devtools -new-window https://reason.com/latest/"
+read-host "When screen scraping is done, press ENTER to continue..."
 Python reasonable.py c:\Users\money\Downloads\reason-latest.csv c:\Users\money\AppData\Local\Temp\reason-latest.html
