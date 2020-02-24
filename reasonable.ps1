cbb5707edc09fa0f49b52595502aaa838a96d8c0
Mike Laursen <35746661+mvlaursen@users.noreply.github.com>
2020-02-23 19:42:23 -0700

    Update reasonable.ps1
    Workflow is messed up if old .csv is there.

--- reasonable.ps1
+++ reasonable.ps1
@@ -1,6 +1,8 @@
+Write-Output "Removing previous .csv file..."
+Remove-Item -path c:\Users\money\Downloads\reason-latest.csv
 Write-Output "Launching Firefox..."
 Start-Process -FilePath "C:\Program Files\Mozilla Firefox\firefox.exe" -ArgumentList "-devtools -new-window https://reason.com/latest/"
 Read-Host "When screen scraping is done, press ENTER to continue..."
-Write-Output "Launching reasonable.py"
+Write-Output "Launching reasonable.py..."
 Python reasonable.py c:\Users\money\Downloads\reason-latest.csv c:\Users\money\AppData\Local\Temp\reason-latest.html
 Write-Ouptut "Done"
